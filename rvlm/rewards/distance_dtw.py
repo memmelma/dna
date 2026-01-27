ef3add600fdabae14623df6ccca23c0800eca9ec
memmelma <marius.memmel@gmx.net>
2026-01-26 16:45:04 -0800

    itakura

--- rvlm/rewards/distance_dtw.py
+++ rvlm/rewards/distance_dtw.py
@@ -15,38 +15,74 @@ def compute_cost_matrix(series: np.ndarray, query: np.ndarray, p: int = 2) -> np
     diff = s[:, None, :] - q[None, :, :]    # (N, M, D)
     return np.power(np.sum(np.abs(diff) ** p, axis=2), 1.0 / p)
     
+# @jit(nopython=True)
+# def compute_acc_cost_matrix(C: np.ndarray, R_cache: np.ndarray = None) -> np.ndarray:
+#     """
+#     Accumulates cost matrix; supports incremental caching
+#     C: cost matrix(N, M)
+#     R_cache: cached accumulated cost matrix (N, M) (optional)
+#     returns: accumulated cost matrix R: (N+1, M+1) with +inf borders and R[0,0]=0
+#     """
+#     N, M = C.shape
+#     # R has one extra row/col for simpler boundaries; R[0, *] and R[*, 0] start at +inf except R[0,0]=0
+#     R = np.full((N + 1, M + 1), np.inf, dtype=C.dtype)
+#     R[0, 0] = 0.0
+
+#     N_start, M_start = 1, 1
+#     if R_cache is not None:
+#         n_cached, m_cached = R_cache.shape
+#         R[:n_cached, :m_cached] = R_cache
+#         # next new column to fill
+#         M_start = m_cached - 1
+
+#     for j in range(M_start, M + 1):
+#         # fill column j for all rows 1..N
+#         for i in range(N_start, N + 1):
+            
+#             # # original: minimum of prev acc cost (down, left, diagonal)
+#             # m = min(R[i - 1, j], R[i, j - 1], R[i - 1, j - 1])
+            
+#             # modified: forces query to progress one step
+#             m = min(R[i - 1, j], R[i - 1, j - 1])
+            
+#             # accumulated cost is prev acc cost + curr cost
+#             R[i, j] = C[i - 1, j - 1] + m
+#     return R
+
 @jit(nopython=True)
-def compute_acc_cost_matrix(C: np.ndarray, R_cache: np.ndarray = None) -> np.ndarray:
+def compute_acc_cost_matrix(C, R_cache=None, alpha=0.5, beta=2.0):
     """
     Accumulates cost matrix; supports incremental caching
+    
+    Itakura parallelogram constraint: only compute if alpha*i <= j <= beta*i
+    alpha: minimum slope, beta: maximum slope
+    e.g., to allow for up to 0.5 slower and 2x faster than the reference trajectory, set alpha=0.5 and beta=2.0
+
     C: cost matrix(N, M)
     R_cache: cached accumulated cost matrix (N, M) (optional)
     returns: accumulated cost matrix R: (N+1, M+1) with +inf borders and R[0,0]=0
     """
-    N, M = C.shape
-    # R has one extra row/col for simpler boundaries; R[0, *] and R[*, 0] start at +inf except R[0,0]=0
-    R = np.full((N + 1, M + 1), np.inf, dtype=C.dtype)
-    R[0, 0] = 0.0
 
+    N, M = C.shape
+    R = np.full((N+1, M+1), np.inf, dtype=C.dtype)
+    R[0,0] = 0.0
+
+    # caching logic as you have it...
     N_start, M_start = 1, 1
     if R_cache is not None:
         n_cached, m_cached = R_cache.shape
         R[:n_cached, :m_cached] = R_cache
-        # next new column to fill
         M_start = m_cached - 1
 
     for j in range(M_start, M + 1):
-        # fill column j for all rows 1..N
-        for i in range(N_start, N + 1):
-            
-            # # original: minimum of prev acc cost (down, left, diagonal)
-            # m = min(R[i - 1, j], R[i, j - 1], R[i - 1, j - 1])
-            
-            # modified: forces query to progress one step
-            m = min(R[i - 1, j - 1], R[i - 1, j])
-            
-            # accumulated cost is prev acc cost + curr cost
-            R[i, j] = C[i - 1, j - 1] + m
+        for i in range(1, N + 1):
+            # slope constraint: only compute if alpha*i <= j <= beta*i
+            if j < alpha * i or j > beta * i:
+                continue
+
+            prev = min(R[i-1, j], R[i, j-1], R[i-1, j-1])
+            R[i, j] = C[i-1, j-1] + prev
+
     return R
 
 class StreamingDTW(StreamingDistanceBase):
@@ -55,7 +91,7 @@ class StreamingDTW(StreamingDistanceBase):
     No implementation of DTW backtracking to get best match, just returns costs and final indices!
     """
 
-    def __init__(self, series, flexible_end: bool = True, normalize_cost: bool = True):
+    def __init__(self, series, flexible_end: bool = True, normalize_cost: bool = True, alpha: float = 0.0, beta: float = 100.0):
         """
         series: list of numpy arrays (M, D)
         flexible_end: whether to allow the last timestep to be matched to any timestep instead of only the last timestep
@@ -63,6 +99,8 @@ class StreamingDTW(StreamingDistanceBase):
         """
         super().__init__(series, normalize_cost)
         self.flexible_end = flexible_end
+        self.alpha = alpha
+        self.beta = beta
         self.reset()
 
     def reset(self):
@@ -88,7 +126,7 @@ class StreamingDTW(StreamingDistanceBase):
             Cs_init = [compute_cost_matrix(s.astype(np.float32), self.query.astype(np.float32)) for s in self.series]
             self.Cs_new = Cs_init
             # accumulated cost for first query
-            self.Rs_new = [compute_acc_cost_matrix(c) for c in Cs_init]
+            self.Rs_new = [compute_acc_cost_matrix(c, alpha=self.alpha, beta=self.beta) for c in Cs_init]
 
         else:
 
@@ -99,13 +137,12 @@ class StreamingDTW(StreamingDistanceBase):
             # append as a new column
             self.Cs_new = [np.concatenate((cn, cp), axis=1) for cn, cp in zip(self.Cs_new, Cs_partial)]
             # accumulated cost for the new cost matrix
-            self.Rs_new = [compute_acc_cost_matrix(cn, R_cache=rn) for cn, rn in zip(self.Cs_new, self.Rs_new)]
+            self.Rs_new = [compute_acc_cost_matrix(cn, R_cache=rn, alpha=self.alpha, beta=self.beta) for cn, rn in zip(self.Cs_new, self.Rs_new)]
 
         if self.flexible_end:
-            costs = [np.min(rn[:, -1]) for rn in self.Rs_new]
-            idcs = [np.argmin(rn[:, -1]) for rn in self.Rs_new]
-            # costs = [np.min(rn[-1:]) for rn in self.Rs_new]
-            # idcs = [np.argmin(rn[-1,:]) for rn in self.Rs_new]
+            # open-end DTW / prefix-DTW
+            costs = [np.min(rn[1:, -1]) for rn in self.Rs_new]
+            idcs = [np.argmin(rn[1:, -1]) for rn in self.Rs_new]
         else:
             costs = [rn[-1, -1] for rn in self.Rs_new]
             idcs = [-1] * len(costs)
@@ -113,17 +150,4 @@ class StreamingDTW(StreamingDistanceBase):
         if self.normalize_cost:
             costs = [float(c) / len(self.query) for c in costs]
 
-        # print(len(self.query))
-        # data = {
-        #     "query": self.query,
-        #     "series": self.series,
-        #     "costs": costs,
-        #     "Rs_new": self.Rs_new,
-        #     "Cs_new": self.Cs_new,
-        #     "idcs": idcs,
-        # }
-        # import pickle
-        # with open("dtw_cache_data.pkl", "wb") as f:
-        #     pickle.dump(data, f)
-
-        return np.array(costs) # , idcs
+        return np.array(costs)
