b54c564bb85a5dafc1dcc58356f1a4ed541ff5ac
memmelma <marius.memmel@gmx.net>
2026-04-03 08:53:20 -0700

    extend api

--- rvlm/functions/api.py
+++ rvlm/functions/api.py
@@ -4,10 +4,10 @@ logging.getLogger("httpx").setLevel(logging.WARNING)
 from rvlm.functions.gemini import call_gemini
 from rvlm.functions.gpt import call_gpt
 
-async def call_api(prompt, video_input=None, img_input=None, thinking_level="MEDIUM", model_id: str = "gemini-3-flash-preview", json_output: bool = False, response_schema=None, include_thoughts=False):
+async def call_api(prompt, video_input=None, img_input=None, thinking_level="MEDIUM", model_id: str = "gemini-3-flash-preview", media_resolution="MEDIA_RESOLUTION_HIGH", json_output: bool = False, response_schema=None, include_thoughts=False):
     if model_id.startswith("gemini"):
-        return await call_gemini(prompt, video_input, img_input, thinking_level, model_id, json_output, response_schema=response_schema, include_thoughts=include_thoughts)
+        return await call_gemini(prompt, video_input, img_input, thinking_level, model_id, media_resolution, json_output, response_schema=response_schema, include_thoughts=include_thoughts)
     elif model_id.startswith("gpt"):
-        return await call_gpt(prompt, video_input, img_input, thinking_level, model_id, json_output, response_schema)
+        return await call_gpt(prompt, video_input, img_input, thinking_level, model_id, media_resolution, json_output, response_schema)
     else:
         raise ValueError(f"Invalid model ID: {model_id}")
